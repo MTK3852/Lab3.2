426c1d382fca0a64018c1ad000dbfd7c3c4c8e62
MTK3852 <mtk835123@gmail.com>
2020-05-06 17:49:22 +0300

    Third lab task3

---
+++ LAB3.2OOP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+
+namespace LAB3._2OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader start = new StreamReader("E:\\C#\\input1.txt");
+            
+            string line = start.ReadLine().Trim();
+            Console.WriteLine(line);
+            string res= brackets(line);
+            Console.WriteLine(res);
+        }
+        static string brackets(string input)
+        {
+            char[] arr = input.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '(')
+                {
+                    int k = i + 1;
+                    while (arr[k] != ')')
+                    {
+                        arr[k] = '\0';
+                        k++;
+                    }
+                }
+            }
+            string result = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != '\0')
+                {
+                    result += arr[i];
+                }
+            }
+            return result;
+        }
+    }
+}
